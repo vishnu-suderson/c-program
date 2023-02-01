255d2cf6e44449f851e57551ee7c7e5da12ea83b
vishnu_suderson <122167792+vishnu-suderson@users.noreply.github.com>
2023-02-01 13:55:21 +0530

    Create number_series.c

---
+++ number_series.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,i;
+	printf("enter the limit of a: ");
+	scanf("%d",&a);
+	printf("\n");
+	for(i=1;i<=a;i++)
+	{
+		if(i==a)
+		printf("%d",i);
+		else
+		printf("%d",i);
+	}
+	getch();
+	return 0;
+}
