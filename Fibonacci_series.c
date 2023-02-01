6e69996479074f6fa03c550223148331af5c6975
vishnu_suderson <122167792+vishnu-suderson@users.noreply.github.com>
2023-02-01 13:52:04 +0530

    Create Fibonacci_series.c

---
+++ Fibonacci_series.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("print all even numbers till: ");
+	scanf("%d",&n);
+	printf("even numbers from 1 to %d are: \n", n);
+	for(i=1; i<=n; i++)
+	{
+		if(i%2 == 0)
+		{
+			printf("%d\n", i);
+		}
+	}
+	return 0;
+}
